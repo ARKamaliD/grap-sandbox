fd28874d52f1fc8118859c6d39c3735f7e4bb6b8
Alireza Kamalidehghan <alireza.kamaldiehghan@tum.de>
2023-05-10 13:06:23 +0200

    Added files for the fourth tutorium

--- w04/xor/main.c
+++ w04/xor/main.c
@@ -1,15 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void xor_cipher(char* str, int key);
+void xor_cipher(char *str, int key);
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv) {
     if (argc != 3) {
         fprintf(stderr, "usage: %s <string> <key>\n", argv[0]);
         return 1;
     }
 
     int key = atoi(argv[2]);
+/*
+    xor_cipher(argv[1], key);
+
+    // Why would "printf(argv[1]);" be a VERY BAD idea?
+    printf("%s\n", argv[1]);
+*/
     xor_cipher(argv[1], key);
 
     // Why would "printf(argv[1]);" be a VERY BAD idea?
@@ -17,4 +23,3 @@ int main(int argc, char** argv) {
 
     return 0;
 }
-
