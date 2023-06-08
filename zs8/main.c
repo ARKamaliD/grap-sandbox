0e6a31bd90d538f1ba7ed9b0a9e4f48737bbcecb
Alireza Kamalidehghan <alireza.kamaldiehghan@tum.de>
2023-06-08 19:41:53 +0200

    added zs8

--- zs8/main.c
+++ zs8/main.c
@@ -1,3 +1,14 @@
-int main(void){
+#include <stdint.h>
+
+struct grasm_state {
+    uint64_t ip, acc, r0, r1, r2, r3, r4, r5, r6, r7;
+};
+
+uint64_t grasm_interpreter(struct grasm_state *state, size_t len, uint8_t prog[len]);
+
+int main(int argc, char **argv) {
+    struct grasm_state state;
+    uint8_t prog[0];
+    grasm_interpreter(&state, 0, prog);
     return 0;
 }
